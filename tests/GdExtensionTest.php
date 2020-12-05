ebc122777a0a9dca4e281e809bb58841567e26d7
Jiri Travnicek <jiri.travnicek@outlook.com>
2020-12-06 00:05:21 +0100

    add php 8, optimize fpm

---
+++ tests/GdExtensionTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests;
+
+use Nette\Utils\Image;
+use PHPUnit\Framework\TestCase;
+
+class GdExtensionTest extends TestCase
+{
+    public function testExtensionAvailable()
+    {
+        self::assertTrue(extension_loaded('gd'), 'extension_loaded reports true for "gd"');
+    }
+
+    /**
+     * @dataProvider dataProviderLoadAndSave
+     */
+    public function testLoadAndSave(int $imageType, string $extension)
+    {
+        $tmpLocation = sys_get_temp_dir() . DIRECTORY_SEPARATOR . getmypid() . '.' . $extension;
+
+        $image = Image::fromFile(__DIR__ . '/logo.png');
+        $image->resize(200, null);
+        $image->save($tmpLocation);
+
+        $resizedImage = Image::fromFile($tmpLocation);
+        self::assertEquals(200, $resizedImage->getWidth());
+    }
+
+    public function dataProviderLoadAndSave()
+    {
+        return [
+            [
+                'imageType' => Image::JPEG,
+                'extension' => 'jpg',
+            ],
+            [
+                'imageType' => Image::PNG,
+                'extension' => 'png',
+            ],
+            [
+                'imageType' => Image::BMP,
+                'extension' => 'bmp',
+            ],
+            [
+                'imageType' => Image::GIF,
+                'extension' => 'gif',
+            ],
+            [
+                'imageType' => Image::WEBP,
+                'extension' => 'webp',
+            ],
+        ];
+    }
+}
